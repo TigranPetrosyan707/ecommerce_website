e2743a4fcb05948e73164d1f25ea3b212191c582
Tigran Petrosyan <tigranpetrosyan.707@gmail.com>
2022-08-21 16:49:48 +0400

    eCommerce webpage

---
+++ fetchingData/booksStore.js
@@ -0,0 +1,74 @@
+export const productBooks = [
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162791.jpg",
+    title: "«Տունս»",
+    author: "Տիգրանյան Ս․",
+    price: "4500 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162774.jpg",
+    title: "«Ակադեմիան և կայսրությունը»",
+    author: "Ազիմով Ա․",
+    price: "5990 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162058.jpg",
+    title: "«Փախուստ Էրգրից»",
+    author: "Զանոյեան Վ․",
+    price: "5800 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162800.jpg",
+    title: "«Լսելով Սեզանին, Դեգային, Ռենուարին»",
+    author: "Վոլլար Ա․",
+    price: "8900 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162799.jpg",
+    title: "«Ամերիքա»",
+    author: "Պըլտեան Գ.",
+    price: "5500 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00161717.jpg",
+    title: "«Աղի թեյ»",
+    author: "Սևադա Ս.",
+    price: "4800 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162766.jpg",
+    title: "«Երբեք մենակ մի ճաշեք և նեթվորքինգի այլ կանոններ»",
+    author: "Ֆերացի Ք. Ռեզ Թ.",
+    price: "5990 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162725.jpg",
+    title: "«Հնչերանգ»",
+    author: "Տերենց Ն․",
+    price: "1350 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162726.jpg",
+    title: "«Երկու դերանուն»",
+    author: "Տերենց Ն․",
+    price: "1350 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162543.jpg",
+    title: "«Կիսաճանապարհին»",
+    author: "Եղիազարյան Գ.",
+    price: "3000 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162728.jpg",
+    title: "«Մարզիր ուղեղդ»",
+    author: "Կավաշիմա Ր․ Գ.",
+    price: "3600 դր․",
+  },
+  {
+    src: "https://books.am/media/catalog/product/cache/46d59aee807c16e1e7589f740f52d186/0/0/00-00162311.jpg",
+    title: "«Իժերի կծիկը»",
+    author: "Մորիակ Ֆր.",
+    price: "3990 դր․",
+  },
+];
---
+++ main.js
@@ -0,0 +1,187 @@
+import { productBooks } from "./fetchingData/booksStore.js";
+const addBasketVoice = new Audio();
+addBasketVoice.src = "./assets/click.wav";
+const getStoreBooksButton = document.querySelector(".fetchingData");
+const basketButton = document.querySelector(".basket");
+
+let sectionForBookLists = document.getElementById("bookListsSection");
+
+const searchInput = document.querySelector(".searchInput");
+
+const getBooks = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productBooks);
+    }, 2000);
+  });
+};
+
+const drawBookLists = () => {
+  getBooks().then((bookList) => {
+    bookList.forEach((book) => {
+      sectionForBookLists.append(createBookList(book));
+    });
+  });
+};
+
+const createBookList = ({ src, title, author, price }) => {
+  let container = document.createElement("article");
+  container.setAttribute("class", "bookCard");
+  let headDiv = document.createElement("div");
+  headDiv.setAttribute("class", "bookCardHeader");
+  let bookCover = document.createElement("img");
+  bookCover.setAttribute("src", `${src}`);
+  headDiv.appendChild(bookCover);
+  let bodyDiv = document.createElement("div");
+  bodyDiv.setAttribute("class", "bookInfo");
+  let bookTitle = document.createElement("p");
+  bookTitle.setAttribute("class", "bookTitle");
+  bookTitle.innerText = title;
+  let bookAuthor = document.createElement("p");
+  bookAuthor.innerText = author;
+  let bookPrice = document.createElement("p");
+  bookPrice.innerText = price;
+  let bookLike = document.createElement("img");
+  bookLike.setAttribute("src", `${"assets/heart.png"}`);
+  bookLike.setAttribute("class", "likeSymbol");
+  bookLike.addEventListener("click", () => {
+    if (bookLike.matches(".likeSymbol")) {
+      bookLike.setAttribute("class", "likeSymbolRed");
+    } else {
+      bookLike.setAttribute("class", "likeSymbol");
+    }
+  });
+  let addBasket = document.createElement("img");
+  addBasket.setAttribute("src", `${"assets/add-to-basket.png"}`);
+  addBasket.setAttribute("class", "addMe");
+  addBasket.addEventListener("click", () => {
+    addBasketVoice.play();
+  });
+  addBasket.addEventListener("click", () => {
+    basketButton.dataset.basketbook++;
+  });
+
+  addBasket.addEventListener("click", (e) => {
+    let bookContent = `
+    <div class = "purchaseBooks"> 
+
+      <div class= "bookBox">
+          <img src= ${src} alt="" class="bookBoxImg" />
+
+          <div class="bookBoxInfo"> 
+            <div class="bookBoxTitle"> ${title} </div>
+            <div class="bookBoxPrice"> ${price} </div>
+          </div>
+
+          <button class="removeBookBox"> DELETE </button>
+      </div>
+
+     </div>
+  `;
+    document
+      .querySelector(".popUpBasket")
+      .insertAdjacentHTML("afterbegin", bookContent);
+
+    let removeBookBox = document.querySelector(".removeBookBox");
+    removeBookBox.addEventListener("click", (e) => {
+      e.target.parentElement.remove();
+    });
+  });
+
+  bodyDiv.append(bookTitle, bookAuthor, bookPrice, bookLike, addBasket);
+  container.append(headDiv, bodyDiv);
+  return container;
+};
+
+getStoreBooksButton.addEventListener("click", drawBookLists);
+
+const searchingBooks = () => {
+  let parentCard = [...document.querySelectorAll(".bookCard")];
+  parentCard = parentCard.map((eachCard) => {
+    if (
+      !eachCard.lastElementChild.firstElementChild.innerText
+        .toLowerCase()
+        .includes(searchInput.value.toLowerCase())
+    ) {
+      eachCard.style.display = "none";
+    } else {
+      eachCard.style.display = "block";
+    }
+    return eachCard;
+  });
+  sectionForBookLists.innerHTML = "";
+  sectionForBookLists.append(...parentCard);
+};
+
+searchInput.addEventListener("keyup", searchingBooks);
+
+const isEmptyInput = () => {
+  if (sectionForBookLists.children.length > 0) {
+    searchInput.removeAttribute("readonly");
+    searchInput.focus();
+  }
+};
+
+searchInput.addEventListener("click", isEmptyInput);
+
+const lazyLoading = () => {
+  let loadingImg = document.createElement("img");
+  loadingImg.setAttribute("src", `${"assets/loading.png"}`);
+  loadingImg.classList.add("loadImg");
+  if (sectionForBookLists.children.length < 1) {
+    sectionForBookLists.append(loadingImg);
+    setTimeout(() => {
+      sectionForBookLists.removeChild(loadingImg);
+    }, 1990);
+  }
+};
+
+getStoreBooksButton.addEventListener("click", lazyLoading);
+
+const createPopUpBasket = () => {
+  let popUpBasket = document.createElement("div");
+  popUpBasket.setAttribute("class", "popUpBasket");
+  let popUpTitle = document.createElement("h3");
+  popUpTitle.setAttribute("class", "popUpTitle");
+  popUpTitle.innerText = "Your collected basket";
+  let closePopUpBasket = document.createElement("button");
+  closePopUpBasket.setAttribute("class", "closePopUpBasket");
+  closePopUpBasket.innerText = "X";
+  let totalPurchases = document.createElement("div");
+  totalPurchases.setAttribute("class", "totalPurchases");
+  let totalTitle = document.createElement("div");
+  totalTitle.setAttribute("class", "total-title");
+  totalTitle.innerText = "Total";
+  let totalPrice = document.createElement("div");
+  totalPrice.setAttribute("class", "total-price");
+  totalPrice.innerText = "0 դր․";
+  totalPurchases.append(totalTitle, totalPrice);
+  let buyBooksButton = document.createElement("button");
+  buyBooksButton.setAttribute("class", "buyBooks");
+  buyBooksButton.innerText = "Buy Now";
+  popUpBasket.append(
+    popUpTitle,
+    closePopUpBasket,
+    totalPurchases,
+    buyBooksButton
+  );
+  document.body.append(popUpBasket);
+};
+
+createPopUpBasket();
+
+basketButton.addEventListener("click", () => {
+  document.querySelector(".popUpBasket").style.right = 0;
+});
+
+const closePopUpBasket = document.querySelector(".closePopUpBasket");
+
+closePopUpBasket.addEventListener("click", () => {
+  document.querySelector(".popUpBasket").style.right = "-100%";
+});
+
+document.addEventListener("click", ({ pageX }) => {
+  if (pageX < 1070) {
+    document.querySelector(".popUpBasket").style.right = "-100%";
+  }
+});
